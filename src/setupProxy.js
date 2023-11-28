9ec912906e00d22ceb73665befe1c98b4acc272b
Jaafar Safar <jaafarsafar@Jaafars-Air.lan>
2023-11-28 15:56:43 -0800

    chore: call api for converting text to image

---
+++ src/setupProxy.js
@@ -0,0 +1,8 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = {
+  target: 'https://mko6b9drb2.execute-api.us-east-1.amazonaws.com/',
+  changeOrigin: true,
+};
+module.exports = function (app) {
+  app.use('/test', createProxyMiddleware(proxy));
+};
