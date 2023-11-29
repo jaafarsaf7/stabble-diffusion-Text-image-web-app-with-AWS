eabd518316f9d139ac4ae725f749c79bf3e1bbaf
Jaafar Safar <jaafarsafar@Jaafars-Air.lan>
2023-11-28 16:10:42 -0800

    chore: call api for converting text to image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Spinner from 'react-bootstrap/Spinner';
